3a36b1662c13bb6c9355e4b4fb1c84488b0ca6fd
Kim Miso <kimms9725@naver.com>
2024-07-22 19:59:13 +0900

    fix alt

--- src/app/components/CustomSticker.tsx
+++ src/app/components/CustomSticker.tsx
@@ -8,19 +8,19 @@ export default function CustomSticker() {
         <p className="font-semibold">최고!</p>
       </div>
       <div>
-        <Image src={'/stickers/good.png'} width={96} height={96} alt="best" />
+        <Image src={'/stickers/good.png'} width={96} height={96} alt="good" />
         <p className="font-semibold">좋아!</p>
       </div>
       <div>
-        <Image src={'/stickers/soso.png'} width={96} height={96} alt="best" />
+        <Image src={'/stickers/soso.png'} width={96} height={96} alt="soso" />
         <p className="font-semibold">그냥저냥</p>
       </div>
       <div>
-        <Image src={'/stickers/bad.png'} width={96} height={96} alt="best" />
+        <Image src={'/stickers/bad.png'} width={96} height={96} alt="bad" />
         <p className="font-semibold">별로...</p>
       </div>
       <div>
-        <Image src={'/stickers/worst.png'} width={96} height={96} alt="best" />
+        <Image src={'/stickers/worst.png'} width={96} height={96} alt="worst" />
         <p className="font-semibold">최악!</p>
       </div>
     </div>
